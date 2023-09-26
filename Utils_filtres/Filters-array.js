80162125dd81b572dc0b4153e8b289614acc93b6
AdrienM2b <adrien.marchetti.6@gmail.com>
2023-09-26 08:46:52 +0200

    changement nom fonction filtre

--- Utils_filtres/Filters-array.js
+++ Utils_filtres/Filters-array.js
@@ -1,94 +1,137 @@
-import { rechercheParTag } from "../algorithme_de_recherche/algo_methode_array.js"
-import { showRecipes, displaySearch } from "../affichage_recette/displayRecipe-array.js"
-import { createHtmlElement, addTagButton } from "./filters-factory-array.js"
+import { rechercheParTag } from '../algorithme_de_recherche/algo_methode_array.js';
+import {
+  showRecipes,
+  displaySearch,
+} from '../affichage_recette/displayRecipe-array.js';
+import { createHtmlElement, addTagButton } from './filters-factory-array.js';
 
-function filtersFactory(recipes, input) {
-    // Créer une constante avec flatMap opur pouvoir l'utiliser dans la fonction extractELements
-    const mappedIngredient = recipes.flatMap(recipes => recipes.ingredients)
+function updateFilters(recipes, input) {
+  // Créer une constante avec flatMap opur pouvoir l'utiliser dans la fonction extractELements
+  const mappedIngredient = recipes.flatMap((recipes) => recipes.ingredients);
 
-    // On defini les différentes listes qu'on remet à zéro à la lecture de la fonction
-    const dataListIngredients = document.querySelector('#list-ingredients')
-    const dataListUstensiles = document.querySelector('#list-ustensils')
-    const dataListAppareils = document.querySelector('#list-appliance')
-    dataListIngredients.innerHTML = ''
-    dataListUstensiles.innerHTML = ''
-    dataListAppareils.innerHTML = ''
-  
-    // On appelle les fonctions qui vont créer les listes
-    const extractElementsIngredients = extractElements(recipes, 'ingredient', dataListIngredients, mappedIngredient)
-    const extractElementsUstensils = extractElements(recipes, 'ustensils', dataListUstensiles)
-    const extractElementsAppliance = extractElements(recipes, 'appliance', dataListAppareils) 
-    
-    // On defini les différentes input des listes 
-    const inputIngredients = document.querySelector('#ingredients')
-    const inputUstensiles = document.querySelector('#ustensils')
-    const inputAppareils = document.querySelector('#appliance')
+  // On defini les différentes listes qu'on remet à zéro à la lecture de la fonction
+  const dataListIngredients = document.querySelector('#list-ingredients');
+  const dataListUstensiles = document.querySelector('#list-ustensils');
+  const dataListAppareils = document.querySelector('#list-appliance');
+  dataListIngredients.innerHTML = '';
+  dataListUstensiles.innerHTML = '';
+  dataListAppareils.innerHTML = '';
 
-    // On appelle les ecouteurs
-    addEventListenerToDropdownItems(recipes, dataListIngredients)
-    addEventListenerToDropdownItems(recipes, dataListUstensiles)
-    addEventListenerToDropdownItems(recipes, dataListAppareils)
+  // On appelle les fonctions qui vont créer les listes
+  const extractElementsIngredients = extractElements(
+    recipes,
+    'ingredient',
+    dataListIngredients,
+    mappedIngredient
+  );
+  const extractElementsUstensils = extractElements(
+    recipes,
+    'ustensils',
+    dataListUstensiles
+  );
+  const extractElementsAppliance = extractElements(
+    recipes,
+    'appliance',
+    dataListAppareils
+  );
 
-    // On appelle les ecouteurs des inputs des listes
-    addEventListenerInputs(recipes, extractElementsIngredients, dataListIngredients, inputIngredients)
-    addEventListenerInputs(recipes, extractElementsUstensils, dataListUstensiles, inputUstensiles)
-    addEventListenerInputs(recipes, extractElementsAppliance, dataListAppareils, inputAppareils)
+  // On defini les différentes input des listes
+  const inputIngredients = document.querySelector('#ingredients');
+  const inputUstensiles = document.querySelector('#ustensils');
+  const inputAppareils = document.querySelector('#appliance');
+
+  // On appelle les ecouteurs
+  addEventListenerToDropdownItems(recipes, dataListIngredients);
+  addEventListenerToDropdownItems(recipes, dataListUstensiles);
+  addEventListenerToDropdownItems(recipes, dataListAppareils);
+
+  // On appelle les ecouteurs des inputs des listes
+  addEventListenerInputs(
+    recipes,
+    extractElementsIngredients,
+    dataListIngredients,
+    inputIngredients
+  );
+  addEventListenerInputs(
+    recipes,
+    extractElementsUstensils,
+    dataListUstensiles,
+    inputUstensiles
+  );
+  addEventListenerInputs(
+    recipes,
+    extractElementsAppliance,
+    dataListAppareils,
+    inputAppareils
+  );
 }
 
 // création des listes en fonction des données reçues
-function extractElements(recipes, type, datalist, mappedIngredient){
-    const uniqueElements = []
-    if (type === 'ingredient') {
-        const justIngredients = mappedIngredient.flatMap(recipe => recipe[type].toString().toLowerCase())
-        const ingredientListReduite = justIngredients.reduce((acc, currentIngredient) => acc.includes(currentIngredient) ? acc : acc.concat(currentIngredient), uniqueElements)
-        uniqueElements.push(ingredientListReduite)
-        return createHtmlElement(ingredientListReduite, datalist)
-    } else{
-        const elements = recipes.flatMap(recipe => recipe[type])
-        const elementsListReduite = elements.reduce((acc, currentElements) => acc.includes(currentElements) ? acc : acc.concat(currentElements), uniqueElements)
-        uniqueElements.push(elementsListReduite)
-        createHtmlElement(elementsListReduite, datalist)
-    }
+function extractElements(recipes, type, datalist, mappedIngredient) {
+  const uniqueElements = [];
+  if (type === 'ingredient') {
+    const justIngredients = mappedIngredient.flatMap((recipe) =>
+      recipe[type].toString().toLowerCase()
+    );
+    const ingredientListReduite = justIngredients.reduce(
+      (acc, currentIngredient) =>
+        acc.includes(currentIngredient) ? acc : acc.concat(currentIngredient),
+      uniqueElements
+    );
+    uniqueElements.push(ingredientListReduite);
+    return createHtmlElement(ingredientListReduite, datalist);
+  } else {
+    const elements = recipes.flatMap((recipe) => recipe[type]);
+    const elementsListReduite = elements.reduce(
+      (acc, currentElements) =>
+        acc.includes(currentElements) ? acc : acc.concat(currentElements),
+      uniqueElements
+    );
+    uniqueElements.push(elementsListReduite);
+    createHtmlElement(elementsListReduite, datalist);
+  }
 
-    return uniqueElements
+  return uniqueElements;
 }
 
-
-// Creation d'un fonction pour factoriser les Listeners sur les listes 
+// Creation d'un fonction pour factoriser les Listeners sur les listes
 function addEventListenerToDropdownItems(recipes, dataList) {
-    // const formContainer = document.querySelector('.forms_container')
-    dataList.querySelectorAll('.dropdown-item').forEach((button, index) => {
-        button.addEventListener('click', () => {
-            const selectedIngredient = button.textContent
-            // Exécutez la recherche avec l'element sélectionné
-            const resultatRecherche = rechercheParTag(recipes, selectedIngredient)
-            // Mettez à jour l'affichage des recettes avec les nouvelles recettes filtrées
-            showRecipes(resultatRecherche, selectedIngredient)
-            filtersFactory(resultatRecherche, selectedIngredient)
-            addTagButton(resultatRecherche, selectedIngredient)
-        })
-    })
+  // const formContainer = document.querySelector('.forms_container')
+  dataList.querySelectorAll('.dropdown-item').forEach((button, index) => {
+    button.addEventListener('click', () => {
+      const selectedIngredient = button.textContent;
+      // Exécutez la recherche avec l'element sélectionné
+      const resultatRecherche = rechercheParTag(recipes, selectedIngredient);
+      // Mettez à jour l'affichage des recettes avec les nouvelles recettes filtrées
+      showRecipes(resultatRecherche, selectedIngredient);
+      updateFilters(resultatRecherche, selectedIngredient);
+      addTagButton(resultatRecherche, selectedIngredient);
+    });
+  });
 }
-
 
 // Fonction ecoute les inputs pour trier la liste des ingredients, appareils, ustensiles
-function addEventListenerInputs(recipes, uniqueElements, datalist, inputDataList) {
-    inputDataList.addEventListener('input', () => {
-        const valueOfinputDataList = inputDataList.value
-        if (valueOfinputDataList.length >= 3) {
-            const filteredElements = uniqueElements.filter((element) => {
-                return element.toLowerCase().includes(valueOfinputDataList);
-            })
-            datalist.innerHTML = ''
-           // Mettre à jour l'affichage de la liste avec les éléments filtrés
-           createHtmlElement(filteredElements, datalist)
-           // Réaffecter les écouteurs
-           addEventListenerToDropdownItems(recipes, datalist)
-        } else if(valueOfinputDataList.length == 0) {
-            createHtmlElement(uniqueElements, datalist)
-        }
-    })
+function addEventListenerInputs(
+  recipes,
+  uniqueElements,
+  datalist,
+  inputDataList
+) {
+  inputDataList.addEventListener('input', () => {
+    const valueOfinputDataList = inputDataList.value;
+    if (valueOfinputDataList.length >= 3) {
+      const filteredElements = uniqueElements.filter((element) => {
+        return element.toLowerCase().includes(valueOfinputDataList);
+      });
+      datalist.innerHTML = '';
+      // Mettre à jour l'affichage de la liste avec les éléments filtrés
+      createHtmlElement(filteredElements, datalist);
+      // Réaffecter les écouteurs
+      addEventListenerToDropdownItems(recipes, datalist);
+    } else if (valueOfinputDataList.length == 0) {
+      createHtmlElement(uniqueElements, datalist);
+    }
+  });
 }
 
-
-export { filtersFactory }
+export { updateFilters };
--- affichage_recette/displayRecipe-array.js
+++ affichage_recette/displayRecipe-array.js
@@ -1,65 +1,63 @@
-import { recherchePrincipale } from "../algorithme_de_recherche/algo_methode_array.js"
-import { filtersFactory } from "../Utils_filtres/Filters-array.js"
-import { recipesFactory } from "./recipe-factory-array.js"
+import { recherchePrincipale } from '../algorithme_de_recherche/algo_methode_array.js';
+import { updateFilters } from '../Utils_filtres/Filters-array.js';
+import { recipesFactory } from './recipe-factory-array.js';
 
-
-const cardContainer = document.querySelector('#card_container')
+const cardContainer = document.querySelector('#card_container');
 function removeTag() {
   const formContainer = document.querySelector('.forms_container');
   const tagButton = formContainer.querySelector('.btn-close');
   if (tagButton) {
-      formContainer.removeChild(tagButton);
+    formContainer.removeChild(tagButton);
   }
 }
 
-function displaySearch(recipes, selectedElement){
-    const valueInput = document.querySelector('.form-control')
-    // J'affiche par défaut les 50 recettes
-    filtersFactory(recipes, selectedElement)
-    showRecipes(recipes, selectedElement)
-    valueInput.addEventListener('input', () => {
-        removeTag()
-        const inputValue = valueInput.value
-        // Je vérifie si la longueur de la chaîne de caractères est d'au moins 3 caractères
-        if (inputValue.length >= 3) {
-            // Je recupère le tri de mon algorithme
-            const resultatRecherche = recherchePrincipale(recipes, inputValue)
-            // J'affiche mes recettes triées et je mets à jour mes filtres
-            filtersFactory(resultatRecherche, valueInput)
-            showRecipes(resultatRecherche, valueInput)
-        } else {
-            // Si la longueur est inférieure à 3, j'efface les résultats
-            cardContainer.innerHTML = ""
-            // J'affiche à nouveau mes 50 recettes et je mets à jour mes filtres
-            filtersFactory(recipes, valueInput)
-            showRecipes(recipes, valueInput)
-        }
-    })
-
-    return valueInput
-}
-
-
-function showRecipes(recipes, valueInput){
-    cardContainer.innerHTML = '' // Effacez le contenu précédent
-
-    if (recipes.length === 0) {
-      // Aucune recette trouvée, affichez le message 
-      cardContainer.innerHTML = " Aucune recette ne contient " + valueInput.value
+function displaySearch(recipes, selectedElement) {
+  const valueInput = document.querySelector('.form-control');
+  // J'affiche par défaut les 50 recettes
+  updateFilters(recipes, selectedElement);
+  showRecipes(recipes, selectedElement);
+  valueInput.addEventListener('input', () => {
+    removeTag();
+    const inputValue = valueInput.value;
+    // Je vérifie si la longueur de la chaîne de caractères est d'au moins 3 caractères
+    if (inputValue.length >= 3) {
+      // Je recupère le tri de mon algorithme
+      const resultatRecherche = recherchePrincipale(recipes, inputValue);
+      // J'affiche mes recettes triées et je mets à jour mes filtres
+      updateFilters(resultatRecherche, valueInput);
+      showRecipes(resultatRecherche, valueInput);
     } else {
-      // Des recettes ont été trouvées
-      recipes.forEach((recipe) => {
-        const getRecipeFact = recipesFactory(recipe)
-        const showTheRecipes = getRecipeFact.recipesDesign()
-        cardContainer.appendChild(showTheRecipes)
-      })
+      // Si la longueur est inférieure à 3, j'efface les résultats
+      cardContainer.innerHTML = '';
+      // J'affiche à nouveau mes 50 recettes et je mets à jour mes filtres
+      updateFilters(recipes, valueInput);
+      showRecipes(recipes, valueInput);
     }
+  });
 
-    // const nbrPromise = nbrOfRecipes() // Obtention de la promesse
-    // const nbr = await nbrPromise
-    // console.log(nbr)
-    // if(nbr < 3){
-    //     cardContainer.style.marginRight = '36%'
+  return valueInput;
 }
 
-export { displaySearch, showRecipes }
+function showRecipes(recipes, valueInput) {
+  cardContainer.innerHTML = ''; // Effacez le contenu précédent
+
+  if (recipes.length === 0) {
+    // Aucune recette trouvée, affichez le message
+    cardContainer.innerHTML = ' Aucune recette ne contient ' + valueInput.value;
+  } else {
+    // Des recettes ont été trouvées
+    recipes.forEach((recipe) => {
+      const getRecipeFact = recipesFactory(recipe);
+      const showTheRecipes = getRecipeFact.recipesDesign();
+      cardContainer.appendChild(showTheRecipes);
+    });
+  }
+
+  // const nbrPromise = nbrOfRecipes() // Obtention de la promesse
+  // const nbr = await nbrPromise
+  // console.log(nbr)
+  // if(nbr < 3){
+  //     cardContainer.style.marginRight = '36%'
+}
+
+export { displaySearch, showRecipes };
