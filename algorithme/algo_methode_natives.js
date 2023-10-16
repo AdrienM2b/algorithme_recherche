f67ef103b72d5cffd4ebf22e36e78467dd70c354
AdrienM2b <adrien.marchetti.6@gmail.com>
2023-10-16 15:41:29 +0200

    algorithme

--- algorithme/algo_methode_natives.js
+++ algorithme/algo_methode_natives.js
@@ -17,76 +17,17 @@ function recherchePrincipale(data, input) {
     const allIngredients = recette.ingredients;
     const titleRecipes = recette.name;
     const descriptionRecipes = recette.description.split(' ');
-
-    // Variable pour suivre si une correspondance a été trouvée dans cette recette
-    let correspondanceTrouvee = false;
-
-    // Une boucle pour traverser tous les ingrédients
-    for (let j = 0; j < allIngredients.length; j++) {
-      // une constante qui récupère 1 par 1 les ingrédients contenu dans le tableau allIngredients
-      // et les transforme en string et minuscule
-      const uniqueIngredients = allIngredients[j].ingredient
-        .toString()
-        .toLowerCase();
-      // On regarde si la fonction renvoie True et on envoie les données au tableau
-      if (
-        correspondanceElements(
-          uniqueIngredients,
-          inputLowerCase,
-          longueurCorrespondance
-        )
-      ) {
-        correspondanceTrouvee = true;
-        break; // Sortir de la boucle interne si une correspondance est trouvée
-      }
+    if (
+      lowerCaseIngredients.includes(inputLowerCase) ||
+      lowerCaseTitre.includes(inputLowerCase) ||
+      lowerCaseDescription.includes(inputLowerCase)
+    ) {
+      resultatRechcerche.push(recette);
     }
-
-    if (!correspondanceTrouvee) {
-      for (let k = 0; k < descriptionRecipes.length; k++) {
-        // une constante qui récupère 1 par 1 les mots contenu dans le texte de la description
-        // et les transforme en minuscule
-        const uniqueDescriptionRecipe = descriptionRecipes[k].toLowerCase();
-        // On regarde si la fonction renvoie True et on envoie les données au tableau
-        if (
-          correspondanceElements(
-            uniqueDescriptionRecipe,
-            inputLowerCase,
-            longueurCorrespondance
-          )
-        ) {
-          correspondanceTrouvee = true;
-          break; // Sortir de la boucle interne si une correspondance est trouvée dans la description
-        }
-      }
-    }
-
-    if (!correspondanceTrouvee) {
-      const titleRecipesLowerCase = titleRecipes.toString().toLowerCase();
-      if (correspondanceElements(titleRecipesLowerCase, inputLowerCase)) {
-        correspondanceTrouvee = true;
-      }
-    }
-
-    if (correspondanceTrouvee) {
-      resultatRecherche.push(recette);
-      // Une correspondance a été trouvée dans cette recette, mais nous ne sortons pas de la boucle principale
-    }
-  }
 
   return resultatRecherche;
 }
 
-function correspondanceElements(chaine, recherche, longueurCorrespondance) {
-  // les élements sont découpés de la valeur 0 à l'input length (minimum 3)
-  // puis stocké dans une valeur
-  const premieresLettresChaine = chaine.substring(0, longueurCorrespondance);
-  const premieresLettresRecherche = recherche.substring(
-    0,
-    longueurCorrespondance
-  );
-  // On compare les deux valeurs et on retourne un
-  return premieresLettresChaine === premieresLettresRecherche;
-}
 
 function rechercheParTag(data, input) {
   // initialisation du nouveau tableau qui va accueuillir les datas triées
