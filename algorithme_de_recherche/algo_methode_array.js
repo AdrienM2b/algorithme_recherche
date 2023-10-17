611259c1af024f906cc7b58c3f3662e08a9266f1
AdrienM2b <adrien.marchetti.6@gmail.com>
2023-10-17 15:56:20 +0200

    changemant algorithme

--- algorithme_de_recherche/algo_methode_array.js
+++ algorithme_de_recherche/algo_methode_array.js
@@ -1,27 +1,35 @@
 function recherchePrincipale(data, input) {
-  // initialisation du nouveau tableau qui va accueuillir les datas triées
-  const resultatRechcerche = [];
+  // initialisation du nouvel ensemble (Set) qui va accueillir les données triées
+  const resultatRecherche = [];
 
   // Tous les résultats du tri sont transformés en minuscules pour éviter les différences de casse
   const inputLowerCase = input.toLowerCase();
   data.forEach((recette) => {
-    // On transforme les données en tableau avec flatMap
-    const lowerCaseIngredients = recette.ingredients.flatMap((recipe) =>
-      recipe.ingredient.toString().toLowerCase()
-    );
+    const arrayOfIngredients = recette.ingredients;
+    let recipeMatch = false; // Pour savoir s'il y a eu au moins une correspondance pour cette recette
+
+    arrayOfIngredients.forEach((listOfIngredient) => {
+      const uniqueIngredient = listOfIngredient.ingredient
+        .toString()
+        .toLowerCase();
+
+      if (uniqueIngredient.includes(inputLowerCase)) {
+        recipeMatch = true;
+      }
+    });
+
     const lowerCaseTitre = recette.name.toString().toLowerCase();
     const lowerCaseDescription = recette.description.toString().toLowerCase();
-    // condition d'affichage des recettes en fonction du tri
+
     if (
-      lowerCaseIngredients.includes(inputLowerCase) ||
+      recipeMatch ||
       lowerCaseTitre.includes(inputLowerCase) ||
       lowerCaseDescription.includes(inputLowerCase)
     ) {
-      resultatRechcerche.push(recette);
+      resultatRecherche.push(recette);
     }
   });
-
-  return resultatRechcerche;
+  return resultatRecherche;
 }
 
 function rechercheParTag(data, input) {
